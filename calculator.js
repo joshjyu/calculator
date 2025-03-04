9a80216207df71ada59602e54198061f891bcfca
joshjyu <jjyu98@gmail.com>
2025-03-03 23:51:06 -0800

    Add functionality to equals button

--- calculator.js
+++ calculator.js
@@ -1,61 +1,21 @@
 const currentDigit = document.querySelectorAll(".digit");
 const operator = document.querySelectorAll(".operator");
+const equals = document.querySelector("#equals");
 
-let firstNumber = "";
-let secondNumber = "";
-let selectedOp = "";
+let firstNumberStr = "";
+let secondNumberStr = "";
+let operatorStr = "";
+let result;
 
 for (i = 0; i < currentDigit.length; i++) {
-  currentDigit[i].addEventListener("click", (event) => {
-    if (secondNumber === "" && selectedOp === "") {
-      firstNumber += event.target.id;
-      console.clear();
-      console.log(
-        "1st num: " +
-          firstNumber +
-          "  2nd num: " +
-          secondNumber +
-          "  operator: " +
-          selectedOp,
-      );
-      return firstNumber;
-    } else if (firstNumber !== "" && selectedOp !== "") {
-      secondNumber += event.target.id;
-      console.clear();
-      console.log(
-        "1st num: " +
-          firstNumber +
-          "  2nd num: " +
-          secondNumber +
-          "  operator: " +
-          selectedOp,
-      );
-      return secondNumber;
-    } else {
-      return;
-    }
-  });
+  currentDigit[i].addEventListener("click", clickDigit);
 }
 
 for (i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", (event) => {
-    if (firstNumber !== "" && secondNumber === "") {
-      selectedOp = event.target.id;
-      console.clear();
-      console.log(
-        "1st num: " +
-          firstNumber +
-          "  2nd num: " +
-          secondNumber +
-          "  operator: " +
-          selectedOp,
-      );
-      return selectedOp;
-    } else {
-      return;
-    }
-  });
+  operator[i].addEventListener("click", clickOperator);
 }
+
+equals.addEventListener("click", calculate);
 
 function addition(a, b) {
   return a + b;
@@ -63,4 +23,86 @@ function addition(a, b) {
 
 function subtraction(a, b) {
   return a - b;
+}
+
+function calculate() {
+  if (secondNumberStr === "") {
+    return;
+  }
+
+  const firstNum = parseFloat(firstNumberStr);
+  const secondNum = parseFloat(secondNumberStr);
+
+  switch (operatorStr) {
+    case "plus":
+      result = addition(firstNum, secondNum);
+      break;
+    case "minus":
+      result = subtraction(firstNum, secondNum);
+      break;
+  }
+
+  return console.log("result = " + result);
+}
+
+function clickDigit(event) {
+  if (secondNumberStr === "" && operatorStr === "") {
+    firstNumberStr += event.target.id;
+    console.clear();
+    console.log(
+      "1st num: " +
+        firstNumberStr +
+        "  2nd num: " +
+        secondNumberStr +
+        "  operator: " +
+        operatorStr,
+    );
+    return firstNumberStr;
+  } else if (result) {
+    result = undefined;
+    secondNumberStr = "";
+    operatorStr = "";
+    firstNumberStr = event.target.id;
+    console.clear();
+    console.log(
+      "1st num: " +
+        firstNumberStr +
+        "  2nd num: " +
+        secondNumberStr +
+        "  operator: " +
+        operatorStr,
+    );
+  } else if (firstNumberStr !== "" && operatorStr !== "" && !result) {
+    secondNumberStr += event.target.id;
+    console.clear();
+    console.log(
+      "1st num: " +
+        firstNumberStr +
+        "  2nd num: " +
+        secondNumberStr +
+        "  operator: " +
+        operatorStr,
+    );
+    return secondNumberStr;
+  } else {
+    return;
+  }
+}
+
+function clickOperator(event) {
+  if (firstNumberStr !== "" && secondNumberStr === "" && !result) {
+    operatorStr = event.target.id;
+    console.clear();
+    console.log(
+      "1st num: " +
+        firstNumberStr +
+        "  2nd num: " +
+        secondNumberStr +
+        "  operator: " +
+        operatorStr,
+    );
+    return operatorStr;
+  } else {
+    return;
+  }
 }
