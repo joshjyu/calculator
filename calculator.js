bf8d20acd6f697f9942d536c18cde08821ecd1bf
joshjyu <jjyu98@gmail.com>
2025-03-04 19:03:30 -0800

    Add functional clear button

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 const currentDigit = document.querySelectorAll(".digit");
 const operator = document.querySelectorAll(".operator");
 const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
 
 let firstNumberStr = "";
 let secondNumberStr = "";
@@ -10,59 +11,11 @@ let result;
 for (i = 0; i < currentDigit.length; i++) {
   currentDigit[i].addEventListener("click", clickDigit);
 }
-
 for (i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", clickOperator);
 }
-
 equals.addEventListener("click", calculate);
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtr(a, b) {
-  return a - b;
-}
-
-function multip(a, b) {
-  return a * b;
-}
-
-function div(a, b) {
-  return a / b;
-}
-
-function calculate() {
-  if (secondNumberStr === "") {
-    return;
-  }
-
-  const firstNum = parseFloat(firstNumberStr);
-  const secondNum = parseFloat(secondNumberStr);
-
-  switch (operatorStr) {
-    case "plus":
-      result = add(firstNum, secondNum);
-      break;
-    case "minus":
-      result = subtr(firstNum, secondNum);
-      break;
-    case "multip":
-      result = multip(firstNum, secondNum);
-      break;
-    case "div":
-      result = div(firstNum, secondNum);
-      break;
-  }
-
-  console.log("result = " + result);
-  result = undefined;
-  firstNumberStr = "";
-  secondNumberStr = "";
-  operatorStr = "";
-  return;
-}
+clear.addEventListener("click", allClear);
 
 function clickDigit(event) {
   if (secondNumberStr === "" && operatorStr === "") {
@@ -136,4 +89,67 @@ function clickOperator(event) {
   } else {
     return;
   }
+}
+
+function allClear() {
+  firstNumberStr = "";
+  secondNumberStr = "";
+  operatorStr = "";
+  result = undefined;
+  console.clear();
+  console.log(
+    "1st num: " +
+      firstNumberStr +
+      "  2nd num: " +
+      secondNumberStr +
+      "  operator: " +
+      operatorStr,
+  );
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtr(a, b) {
+  return a - b;
+}
+
+function multip(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function calculate() {
+  if (secondNumberStr === "") {
+    return;
+  }
+
+  const firstNum = parseFloat(firstNumberStr);
+  const secondNum = parseFloat(secondNumberStr);
+
+  switch (operatorStr) {
+    case "plus":
+      result = add(firstNum, secondNum);
+      break;
+    case "minus":
+      result = subtr(firstNum, secondNum);
+      break;
+    case "multip":
+      result = multip(firstNum, secondNum);
+      break;
+    case "div":
+      result = div(firstNum, secondNum);
+      break;
+  }
+
+  console.log("result = " + result);
+  result = undefined;
+  firstNumberStr = "";
+  secondNumberStr = "";
+  operatorStr = "";
+  return;
 }
